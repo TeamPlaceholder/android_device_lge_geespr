20ea5acd9f89eab5310a35f8042172c9e8d5bfbb
xboxfanj <xboxfanj@msn.com>
2013-05-20 18:18:44 -0500

    More gee_spr changes
    Change-Id: I56342ec0c44da033126824ddb812c4a1e64f9179

--- cm.mk
+++ cm.mk
@@ -15,7 +15,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/lge/gee_spr/full_gee_spr_spr_us.mk)
+$(call inherit-product, device/lge/gee_spr/full_gee_spr.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gee_spr
